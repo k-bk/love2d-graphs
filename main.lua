64e45d37ba0792a3ca016fda4ea721e0c4ddbf29
Karol Bąk <bak.karolek@gmail.com>
2019-10-29 17:14:53 +0100

    Cleanup main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,6 @@
 local v2 = require "v2"
 local graph = require "graph"
 
-function rand_double(min, max)
-   return love.math.random() * (max-min) + min
-end
-function int(num) return math.floor(num) end
-
 function love.load()
    points = {}
    points.color = { 1,0,0 } 
@@ -14,9 +9,6 @@ function love.load()
       local deg = love.math.random() * 2 * math.pi
       table.insert(points, v2(100 * math.cos(deg), 100 * math.sin(deg)))
    end
-end
-
-function love.update(dt)
 end
 
 function love.draw()
